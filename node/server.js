ea6811b84b607b588b7f52ee2f5a04198db434fe
Casey James <casey@khaosophy.com>
2021-01-07 11:19:05 -0500

    added cors

--- node/server.js
+++ node/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const colors = require('colors');
@@ -32,6 +33,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
