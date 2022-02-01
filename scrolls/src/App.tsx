d185cd56a5a5bb6113015db34092b1a4c568c561
Casey James Perno <casey@khaosophy.com>
2022-02-01 14:38:13 -0500

    removed create-react-app styles

--- scrolls/src/App.tsx
+++ scrolls/src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ScrollForm from './components/ScrollForm';
 import ScrollPreview  from './components/ScrollPreview';
 
-import './App.css';
-
 function App() {
   return (
     <div className="App">
