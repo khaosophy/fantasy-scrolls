70db3177442f097679376719d332fbbe7ce7abfa
Casey James Perno <casey@khaosophy.com>
2022-02-01 11:23:58 -0500

    skeleton markup

---
+++ scrolls/src/components/ScrollForm.tsx
@@ -0,0 +1,15 @@
+const ScrollForm = () => (
+  <form>
+    <textarea>
+      Your Message
+    </textarea>
+
+    <input placeholder="Font" />
+
+    <input placeholder="Background" />
+
+    <input type="submit" value="Download Image" />
+  </form>
+);
+
+export default ScrollForm;
---
+++ scrolls/src/components/ScrollPreview.tsx
@@ -0,0 +1,7 @@
+const ScrollPreview = () => (
+  <div style={{ height: '100px', width: '100px', backgroundColor: 'red' }}>
+    {/* placeholder */}
+  </div>
+);
+
+export default ScrollPreview;
