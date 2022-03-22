c983160db9cc450ae3eebc1c7a722d50d044c5f5
Casey James Perno <casey@khaosophy.com>
2022-03-22 10:08:15 -0400

    increase default font size from 16 to 20

--- scrolls/src/App.tsx
+++ scrolls/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [message, setMessage] = useState('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
   const [background, setBackground] = useState('Y1.png');
   const [font, setFont] = useState('royal');
-  const [fontSize, setFontSize] = useState('16');
+  const [fontSize, setFontSize] = useState('20');
   const scroll = useRef(null);
 
   const downloadImage = (): void => {
