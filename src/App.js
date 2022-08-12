8ef269287149180c3002182e41e78efdbf64b53b
Casey James Perno <casey@khaosophy.com>
2022-08-12 16:03:01 -0400

    add navigation to header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import ScrollMaker from './components/ScrollMaker';
@@ -7,12 +7,10 @@ function App() {
   return (<>
     <Header />
     <main className="container">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<h1>Casper's Tabletop Tools</h1>} />
-          <Route path="/scroll" element={<ScrollMaker />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<></>} />
+        <Route path="/scroll" element={<ScrollMaker />} />
+      </Routes>
     </main>
     <Footer />
   </>);
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,32 @@
-const Header = () => (
-  <header></header>
-);
+import { Link } from 'react-router-dom';
 
-export default Header;
+export default function Header() {
+  const navItems = [
+    {
+      label: 'Home',
+      href: '/',
+    },
+    {
+      label: 'Scroll Maker',
+      href: '/scroll',
+    },
+  ];
+
+  return (
+    <header className="py-3 mb-4 border-bottom">
+      <div className="container d-flex flex-wrap justify-content-between align-items-center">
+        <h1 className="m-0">Casper's Tabletop Tools</h1>
+        
+        <nav className="ms-auto">
+          <ul className="nav nav-pills">
+            {navItems.map(item => (
+              <li className="nav-item" key={item.href}>
+                <Link to={item.href} className="nav-link">{item.label}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
