119071865fd03548c6910fc8ae034f51229eca2d
Casey James Perno <casey@khaosophy.com>
2022-08-15 12:08:41 -0400

    spell filtering UI

--- src/components/SpellScroll.js
+++ src/components/SpellScroll.js
@@ -1,26 +1,69 @@
 import { useQuery, gql } from '@apollo/client';
+import SelectField from './SelectField';
 
 export default function SpellScroll() {
-  const { data, loading, error } = useQuery(GET_SPELLS);
+  const { data: spellsData, loading: spellsLoading, error: spellsError } = useQuery(GET_SPELLS);
+  const { data: classesData, loading: classesLoading, error: classesError } = useQuery(GET_CLASSES);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error</p>;
+  if (spellsLoading || classesLoading) return <p>Loading...</p>;
+  if (spellsError || classesError) return <p>Error</p>;
 
-  const { spells } = data;
+  const { spells } = spellsData;
+  const { classes } = classesData;
+
+  const handleScrollGenerator = (e) => {
+    e.preventDefault();
+    console.log('selecting a spell...');
+  }
+  
   return (
     <div>
       <h1>Generate a Random Spell Scroll</h1>
-      <ul>
+
+      <form onSubmit={(e) => handleScrollGenerator(e)}>
+        <div className="row mb-2">
+          <div className="col-6">
+            <SelectField
+              id="scrollClassSelect"
+              label="Select a Class"
+              options={[
+                {value: 'any', label: 'Any Class'},
+                ...classes,
+              ]}
+            />
+          </div>
+          <div className="col-6">
+            <SelectField
+              id="scrollLevelSelect"
+              label="Select a Spell Level"
+              options={Array.from({length: 9}, (_, i) => ({ value: i+1, label: `${i + 1}` }))}
+            />
+          </div>
+        </div>
+
+        <button type="submit" className="btn btn-primary">Generate</button>
+      </form>
+
+      {/* <ul>
         {spells.map(spell => (
           <li key={spell.index}>{spell.name}</li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 }
 
+const GET_CLASSES = gql`
+  query Classes {
+    classes {
+      value: index
+      label: name
+    }
+  }
+`
+
 const GET_SPELLS = gql`
-  query GetSpells {
+  query Spells {
     spells {
       index
       name
