4f613308afc3a8b1ca1fc012d0fb377a8cc0ebe4
Casey James Perno <casey@khaosophy.com>
2022-08-23 11:33:01 -0400

    added mobile menu to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,8 @@
 import { Link } from 'react-router-dom';
+import 'bootstrap/js/dist/collapse.js'
 
 export default function Header() {
   const navItems = [
-    {
-      label: 'Home',
-      href: '/',
-    },
     {
       label: 'Handout Maker',
       href: '/handout',
@@ -17,18 +14,28 @@ export default function Header() {
   ];
 
   return (
-    <header className="py-3 mb-4 border-bottom">
-      <div className="container d-flex flex-wrap justify-content-between align-items-center">
-        <h1 className="m-0">Casper's Tabletop Tools</h1>
-        
-        <nav className="ms-auto">
-          <ul className="nav nav-pills">
-            {navItems.map(item => (
-              <li className="nav-item" key={item.href}>
-                <Link to={item.href} className="nav-link">{item.label}</Link>
-              </li>
-            ))}
-          </ul>
+    <header className="py-2 mb-4 border-bottom">
+      <div className="container">
+        <nav className="navbar navbar-expand-sm d-flex justify-content-between">
+          <Link to="/" className="navbar-brand">Casper's Tabletop Tools</Link>
+
+          <button
+            className="btn btn-primary-outline d-sm-none"
+            data-bs-toggle="collapse"
+            data-bs-target="#mainMenu"
+          >
+            Menu
+          </button>
+
+          <div id="mainMenu" className="collapse navbar-collapse flex-grow-0">
+            <ul className="navbar-nav nav nav-pills">
+              {navItems.map(item => (
+                <li className="nav-item" key={item.href}>
+                  <Link to={item.href} className="nav-link">{item.label}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
         </nav>
       </div>
     </header>
