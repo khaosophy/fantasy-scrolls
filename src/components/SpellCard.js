efb93d7e6bfd7ca6ba72f5a524fd816573a3eecc
Casey James Perno <casey@khaosophy.com>
2022-09-09 16:09:00 -0400

    spell card: add levels and class lists

--- src/components/SpellCard.js
+++ src/components/SpellCard.js
@@ -1,19 +1,31 @@
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 export default function SpellCard(props) {
   /**
    * TODO: 
    * * when a description has a list, convert to an actual list? see Conjure Animals
    * * when a description has a table, convert to an actual table? see Confusion
-   * * add class lists
    * * add school
-   * * add level
    * * add damage info
    * * add concentration and other meta info
    */
   return (
-    <div className="spell-card">
+    <div className={classnames(
+      'spell-card',
+      props.className,
+    )}>
       <h3 className="spell-card__title">{props.name}</h3>
+      <div className="spell-card__meta d-flex gap-1">
+        <span className="badge bg-info text-dark">
+          {(props.level > 0) ? (
+            `Level ${props.level}`
+          ) : (
+            `Cantrip`
+          )}
+        </span>
+        {props.lists.map(list => <span key={list.name} className="badge bg-secondary">{list.name}</span>)}
+      </div>
       <div className="spell-card__description">
         {props.description.map((content, i) => (
           <p key={i}>{content}</p>
@@ -24,6 +36,9 @@ export default function SpellCard(props) {
 }
 
 SpellCard.propTypes = {
+  className: PropTypes.string,
   name: PropTypes.string.isRequired,
-  description: PropTypes.arrayOf(PropTypes.string),
+  description: PropTypes.arrayOf(PropTypes.string).isRequired,
+  lists: PropTypes.array.isRequired,
+  level: PropTypes.number.isRequired,
 }
