273dbb824a99b017c96eb330caf6350632dfd9d5
Casey James Perno <casey@khaosophy.com>
2022-09-13 21:01:15 -0400

    remove finished TODO

--- src/api/getSpells.js
+++ src/api/getSpells.js
@@ -1,9 +1,5 @@
 import { gql } from '@apollo/client';
 
-/**
- * todo: how to pass in variables?
- * I want to call this from SpellTable
- */
 const GET_SPELLS = gql`
   query Spells ($name: String) {
     spells (limit: 500, name: $name) {
