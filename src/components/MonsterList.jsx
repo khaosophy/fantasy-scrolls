2dc33a6fa0322c32cc9b2b8f5a37560d4fdcb6c2
Casey Perno <caseyp@partyrentalltd.com>
2023-06-14 15:06:29 -0400

    monster list: search by name

--- src/components/MonsterList.jsx
+++ src/components/MonsterList.jsx
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { useQuery } from '@apollo/client';
 import GET_MONSTERS from '../api/getMonsters';
+import TextField from './TextField';
 
 export default function MonsterList() {
-  const { loading, data: { monsters } } = useQuery(GET_MONSTERS);
+  const [searchQuery, setSearchQuery] = useState('');
+  const { loading, data: { monsters }, refetch } = useQuery(GET_MONSTERS);
   
   if(loading) return (<h3>On the hunt for monsters...</h3>);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    refetch({ name: searchQuery });
+  }
 
   return (
     <div className="mt-3 mb-4">
@@ -14,6 +22,18 @@ export default function MonsterList() {
         <meta name="description" content="Peruse the monsters of the multiverse with these easy-to-use filters." />
       </Helmet>
       <h1>Monsters</h1>
+
+      <form onSubmit={handleSubmit}>
+        <TextField 
+          label="Look up a monster"
+          id="monsterSearchQuery"
+          className="mb-2"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          autoFocus
+        />
+        <button type="submit" className="btn btn-primary">Search</button>
+      </form>
 
       {/* todo: paginate */}
       {(monsters.length > 0) ? (
