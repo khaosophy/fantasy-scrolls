ad508dac2acd8bfdc383cdb1812a9ff85cc3aeeb
Casey James <casey@khaosophy.com>
2023-06-16 23:37:51 -0400

    format low CR has appropriate fraction

--- src/components/MonsterDescription.jsx
+++ src/components/MonsterDescription.jsx
@@ -2,7 +2,7 @@ import { useQuery } from '@apollo/client';
 import { useParams } from 'react-router-dom';
 import GET_MONSTER from '../api/getMonster';
 import { capitalize } from '../utils/text-utils';
-import { formatInteger } from '../utils/number-utils';
+import { formatInteger, formatChallengeRating } from '../utils/number-utils';
 
 export default function MonsterDescription() {
   const { monster: index } = useParams();
@@ -175,8 +175,7 @@ export default function MonsterDescription() {
       <div className="d-flex gap-5">
         <div>
           <dt>Challenge Rating</dt>
-          {/* todo: format low challenge ratings to their factional value */}
-          <dd>{monster.challenge_rating} ({formatInteger(monster.xp)} XP)</dd>
+          <dd>{formatChallengeRating(monster.challenge_rating)} ({formatInteger(monster.xp)} XP)</dd>
         </div>
         {/* todo: proficency bonus? see DndBeyond. it may not be in the API though, and it's not present on Roll20*/}
       </div>
--- src/utils/number-utils.js
+++ src/utils/number-utils.js
@@ -2,3 +2,12 @@ export const formatInteger = (number) => {
   // take a number and return a string with commas
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
+export const formatChallengeRating = (cr) => {
+  // if CR is less than 1, return a fraction as a string
+  // for example, .5 would return "1/2" or .25 would return "1/4"
+  if (cr < 1) {
+    return `1/${1/cr}`
+  }
+  return cr.toString();
+}
