3bd6960c5811f2a1703d3e8680d2f0beb4ce60b6
Casey James Perno <casey@khaosophy.com>
2023-06-20 21:00:23 -0400

    styled separators on Monster Description page

--- src/components/MonsterDescription.jsx
+++ src/components/MonsterDescription.jsx
@@ -87,9 +87,11 @@ export default function MonsterDescription() {
   }
 
   return (<>
-    <h3>{monster.name}</h3>
+    <h3 className="mb-0">{monster.name}</h3>
     {/* todo: monster alignment */}
     <p>{capitalize(monster.size)} {capitalize(monster.type)}</p>
+
+    <hr className="stat-separator" />
 
     <dl>
       {/* todo: AC is an array... what are some cases where it has more than one? */}
@@ -101,6 +103,8 @@ export default function MonsterDescription() {
 
       {renderSpeed()}
     </dl>
+
+    <hr className="stat-separator" />
 
     <dl className="d-flex gap-3">
       <div className="text-center">
@@ -133,6 +137,8 @@ export default function MonsterDescription() {
         <dd>{monster.charisma} ({modifierFromAbilityScore(monster.charisma)})</dd>
       </div>
     </dl>
+
+    <hr className="stat-separator" />
     
     <dl>
       {renderSkillsAndSaves()}
@@ -182,7 +188,7 @@ export default function MonsterDescription() {
       </div>
     </dl>
 
-    {/* todo: style sections more appropriately (i.e. red borders or something) */}
+    <hr className="stat-separator" />
 
     {/* Special Abilities */}
     {monster.special_abilities && (<>
@@ -198,7 +204,7 @@ export default function MonsterDescription() {
       </ul>
     </>)}
 
-    <h4>Actions</h4>
+    <h4 style={{ borderBottom: '1px solid #a52a2a' }}>Actions</h4>
     <ul className="list-unstyled">
       {monster.actions.map(action => (
         <li key={action.name}>
@@ -211,7 +217,7 @@ export default function MonsterDescription() {
     </ul>
 
     {monster.legendary_actions && (<>
-      <h4>Legendary Actions</h4>
+      <h4 style={{ borderBottom: '1px solid #a52a2a' }}>Legendary Actions</h4>
       <ul className="list-unstyled">
         {monster.legendary_actions.map(action => (
           <li key={action.name}>
