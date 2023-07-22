ab5d4c81c8ccadc9ec25f4f032e859fc8422e31e
Casey Perno <casey@khaosophy.com>
2023-07-21 22:39:26 -0400

    capitalize util: handle multiple words

--- src/utils/text-utils.js
+++ src/utils/text-utils.js
@@ -3,6 +3,5 @@ export function capitalize(string) {
    * Take a string and capitalize the first letter of each word.
    * It should also lowercase the rest of the letters.
    */
-  // todo: work with multiple words
-  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+  return string.toLowerCase().replace(/\b\w/g, (l) => l.toUpperCase());
 }
